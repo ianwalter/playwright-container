ed9e17b59880431530458ab99cd099a22e156428
Ian Walter <pub@ianwalter.dev>
2021-08-24 22:55:19 -0400

    Updating bff to v11

--- tests.js
+++ tests.js
@@ -1,6 +1,7 @@
 import { test } from '@ianwalter/bff'
 
 test('My personal site', async t => {
-  await t.page.goto('https://ianwalter.dev')
-  t.expect(await t.page.innerText('body')).toContain('Ian Walter')
+  const { page } = await t.chromium({ args: ['--no-sandbox'] })
+  await page.goto('https://ianwalter.dev')
+  t.expect(await page.innerText('body')).toContain('Ian Walter')
 })
